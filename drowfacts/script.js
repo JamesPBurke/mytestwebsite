57ec45959fb475e1f48a12a9e669381d974c65d4
Dr. James Burke <burkej@sbregional.org>
2020-07-23 16:42:31 +0000

    First working version.

--- drowfacts/script.js
+++ drowfacts/script.js
@@ -1,16 +1,39 @@
-function getText(){
-    // read text from URL location
-    var request = new XMLHttpRequest();
-    request.open('GET', '/facts.txt', true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                return request.responseText;
-            }
-        }
-    }
+/*async function getSampleText() {
+  console.log( (await fetch('facts.txt')).text() );
+  console.log( (await fetch('salutations.txt')).text() );
+}*/
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-document.write(getText());
+Promise.all([
+  fetch('facts.txt').then(x => x.text()),
+  fetch('salutations.txt').then(x => x.text())
+]).then(([facttext, salutext]) => {
+
+factlist = facttext.split("\n");
+salulist = salutext.split("\n");
+
+//document.write("<p>"+facttext+"</p>");
+rndsalu = salulist[getRndInteger(0, salulist.length-1)];
+rndfact = factlist[getRndInteger(0, factlist.length-1)];
+
+/*for (fact of factlist) {
+  document.write("<p>"+fact+"</p>");
+}*/
+
+rndexclaimcount = getRndInteger(1,5);
+rndexclaim = "";
+for (i = 1; i<=rndexclaimcount; i++) {
+  rndexclaim = rndexclaim + "!"
+}
+
+document.write("<p>"+rndsalu+"\n"+rndfact+" Thanks for using DrowFacts"+rndexclaim+"</p>");
+});
+
+
+
+//getSampleText()
+
+/*document.write(doGET("facts.txt"));*/
