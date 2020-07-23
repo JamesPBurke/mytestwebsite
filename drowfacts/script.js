a5e95b02cc1d06049ea5524b343b82f2ce0acc87
Dr. James Burke <burkej@sbregional.org>
2020-07-23 14:52:19 +0000

    Added a list of facts and salutations

---
+++ drowfacts/script.js
@@ -0,0 +1,16 @@
+function getText(){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', '/facts.txt', true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+        }
+    }
+}
+
+document.write(getText());
